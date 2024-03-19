82c35c8ddaba33db5afd33749c5dd3ce71bc9bae
Ahnjaemin1208 <michael1208@naver.com>
2024-03-19 22:41:41 +0900

    Feat : 스프링 데이터 JPA 적용
    JPA 적용 / 스프링 데이터 JPA 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,36 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//
+//        //return new JpaMemberRepository(em);
+//
+//    }
+
+
 }
--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -38,7 +38,7 @@ public class MemberController {
     public String list(Model model){
         List<Member> members = memberService.findMembers();
         model.addAttribute("members",  members);
-        return "members/memverList";
+        return "memberList";
     }
 
 
